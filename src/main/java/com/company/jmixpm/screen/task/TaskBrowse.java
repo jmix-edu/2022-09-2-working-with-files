7f3f804c3e247f333eec3ea8c99f8f0cfe9525f1
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-09-06 16:30:53 +0400

    Completed

--- src/main/java/com/company/jmixpm/screen/task/TaskBrowse.java
+++ src/main/java/com/company/jmixpm/screen/task/TaskBrowse.java
@@ -1,13 +1,95 @@
 package com.company.jmixpm.screen.task;
 
+import com.company.jmixpm.entity.Project;
 import com.company.jmixpm.entity.Task;
+import com.company.jmixpm.entity.User;
+import io.jmix.core.DataManager;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.ui.UiComponents;
+import io.jmix.ui.component.*;
+import io.jmix.ui.download.Downloader;
+import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.screen.*;
 import io.jmix.ui.screen.LookupComponent;
-import io.jmix.ui.screen.StandardLookup;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.upload.TemporaryStorage;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @UiController("Task_.browse")
 @UiDescriptor("task-browse.xml")
 @LookupComponent("tasksTable")
 public class TaskBrowse extends StandardLookup<Task> {
+
+    @Autowired
+    private UiComponents uiComponents;
+
+    @Autowired
+    private Downloader downloader;
+
+    @Autowired
+    private FileStorageUploadField uploadTasks;
+
+    @Autowired
+    private TemporaryStorage temporaryStorage;
+
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @Autowired
+    private CollectionLoader<Task> tasksDl;
+
+    @Install(to = "tasksTable.attachment", subject = "columnGenerator")
+    private Component tasksTableAttachmentColumnGenerator(Task task) {
+        if (task.getAttachment() != null) {
+            LinkButton linkButton = uiComponents.create(LinkButton.class);
+            linkButton.setCaption(task.getAttachment().getFileName());
+            linkButton.addClickListener(clickEvent -> {
+                downloader.download(task.getAttachment());
+            });
+            return linkButton;
+        }
+        return null;
+    }
+
+    @Subscribe("uploadTasks")
+    public void onUploadTasksFileUploadSucceed(SingleFileUploadField.FileUploadSucceedEvent event) throws IOException {
+        UUID fileId = uploadTasks.getFileId();
+        File file = temporaryStorage.getFile(fileId);
+
+        List<String> taskNames = FileUtils.readLines(file, StandardCharsets.UTF_8);
+
+        List<Task> importedTasks = new ArrayList<>(taskNames.size());
+
+        for (String name : taskNames) {
+            Task task = dataManager.create(Task.class);
+            task.setName(name);
+            task.setAssignee((User) currentAuthentication.getUser());
+            task.setProject(loadDefaultProject());
+
+            importedTasks.add(task);
+        }
+
+        dataManager.save(importedTasks.toArray());
+        tasksDl.load();
+    }
+
+    @Nullable
+    private Project loadDefaultProject() {
+        return dataManager.load(Project.class)
+                .query("select p from Project p where p.defaultProject = :defaultProject")
+                .parameter("defaultProject", true)
+                .optional().orElse(null);
+    }
+    
+
 }
--- src/main/java/com/company/jmixpm/screen/task/TaskEdit.java
+++ src/main/java/com/company/jmixpm/screen/task/TaskEdit.java
@@ -2,6 +2,9 @@ package com.company.jmixpm.screen.task;
 
 import com.company.jmixpm.app.TaskService;
 import com.company.jmixpm.entity.Task;
+import io.jmix.ui.component.BrowserFrame;
+import io.jmix.ui.component.FileStorageResource;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,11 +12,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 @UiDescriptor("task-edit.xml")
 @EditedEntityContainer("taskDc")
 public class TaskEdit extends StandardEditor<Task> {
+
+    @Autowired
+    private BrowserFrame attachmentBrowserFrame;
     @Autowired
     private TaskService taskService;
 
     @Subscribe
     public void onInitEntity(InitEntityEvent<Task> event) {
         event.getEntity().setAssignee(taskService.findLeastBusyUser());
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        setAttachmentToFrame();
+    }
+
+    @Subscribe(id = "taskDc", target = Target.DATA_CONTAINER)
+    public void onTaskDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Task> event) {
+        if ("attachment".equals(event.getProperty())) {
+            setAttachmentToFrame();
+        }
+    }
+
+    private void setAttachmentToFrame() {
+        Task task = getEditedEntity();
+        if (task.getAttachment() != null) {
+            attachmentBrowserFrame.setSource(FileStorageResource.class)
+                    .setFileReference(task.getAttachment())
+                    .setMimeType(task.getAttachment().getContentType());
+        }
     }
 }
